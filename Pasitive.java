4f7192ed9d03b87951e563d2e60ed4c8c5b65259
udhayakumar-p <udhayakumarp505@gmail.com>
2017-06-23 17:05:46 +0530

    Create Pasitive.java

---
+++ Pasitive.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Pasitive
+{
+public static void main(String arr[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a==0)
+{
+System.out.print("zero");
+}
+if(a<0)
+{
+System.out.print("positive");
+}
+else
+{
+System.out.print("negative");
+}
+}
+}
