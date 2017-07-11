1681fc94faec708219b9565d7d65cadb94044f38
udhayakumar-p <udhayakumarp505@gmail.com>
2017-07-11 11:26:32 +0530

    Create whether.java

---
+++ whether.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class whether
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a%2==0)
+{
+System.out.print("even");
+}
+else if(a%2!=0)
+{
+System.out.print("odd");
+}
+}
+}
